b1080ff4c30bb9f5f502312c3fdb873d292c3b55
Nasim <mdnasimhosen333@gmail.com>
2025-06-12 21:06:36 +0600

    Add Sort It solution for HackerRank final exam

---
+++ Sort_It.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string nm;
+    int cls;
+    char s;
+    int id;
+    int math_marks;
+    int eng_marks;
+};
+
+bool cmp(Student l, Student r)
+{
+    int sum_l = l.math_marks + l.eng_marks;
+    int sum_r = r.math_marks + r.eng_marks;
+    if (sum_l != sum_r)
+    {
+        return sum_l > sum_r;
+    }
+    else
+    {
+        return l.id < r.id;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].nm >> a[i].cls >> a[i].s >> a[i].id >> a[i].math_marks >> a[i].eng_marks;
+    }
+    sort(a, a + n, cmp);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].nm << " " << a[i].cls << " " << a[i].s << " " << a[i].id << " " << a[i].math_marks << " " << a[i].eng_marks << endl;
+    }
+    return 0;
+}
